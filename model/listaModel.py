87e96015fc3df4bdcfcecb0aa3db047bf6f264a8
AbraaoRSouza <86445184+AbraaoRSouza@users.noreply.github.com>
2023-11-12 11:34:32 -0300

    adição de try_catch

--- model/listaModel.py
+++ model/listaModel.py
@@ -2,25 +2,34 @@ import sqlite3
 class ListaModel():
 
     def cadastrar_lista(lista):
-        con = sqlite3.connect("BD-ListaDeTarefa.db")
-        cur = con.cursor()
-        insert= "INSERT INTO LISTA VALUES(?,?,?);"        
-        cur.execute(insert,(lista.id,lista.descricao,lista.usuario_id)) 
-        con.commit()
-        cur.close()
+        try:
+            con = sqlite3.connect("BD-ListaDeTarefa.db")
+            cur = con.cursor()
+            insert= "INSERT INTO LISTA VALUES(?,?,?);"        
+            cur.execute(insert,(lista.id,lista.descricao,lista.usuario_id)) 
+            con.commit()
+            cur.close()
+        except Exception as e:
+            print(f"Erro ao cadastrar lista: {str(e)}")
 
     def editar_lista(lista):
-        con = sqlite3.connect("BD-ListaDeTarefa.db")
-        cur = con.cursor()
-        update= "UPDATE LISTA set descricao=? WHERE id=?;"        
-        cur.execute(update,(lista.descricao,lista.id)) 
-        con.commit()
-        cur.close()
+        try:
+            con = sqlite3.connect("BD-ListaDeTarefa.db")
+            cur = con.cursor()
+            update= "UPDATE LISTA set descricao=? WHERE id=?;"        
+            cur.execute(update,(lista.descricao,lista.id)) 
+            con.commit()
+            cur.close()
+        except Exception as e:
+            print(f"Erro ao editar lista: {str(e)}")
 
     def excluir_lista(lista):
-        con = sqlite3.connect("BD-ListaDeTarefa.db")
-        cur = con.cursor()
-        insert= "DELETE FROM LISTA WHERE id=?;"        
-        cur.execute(insert,(lista.id)) 
-        con.commit()
-        cur.close()
+        try:
+            con = sqlite3.connect("BD-ListaDeTarefa.db")
+            cur = con.cursor()
+            insert= "DELETE FROM LISTA WHERE id=?;"        
+            cur.execute(insert,(lista.id)) 
+            con.commit()
+            cur.close()
+        except Exception as e:
+            print(f"Erro ao excluir lista: {str(e)}")
